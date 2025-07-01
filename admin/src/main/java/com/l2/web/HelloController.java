a9ab27b233cd94e660eab0390c449bc9e1147fab
LitchiXian <3468845278@qq.com>
2025-07-01 14:34:26 +0800

    Init

---
+++ admin/src/main/java/com/l2/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.l2.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello world";
+    }
+
+}
