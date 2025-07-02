1205278163b4e2a3c1be5ddc53af75563c5db613
Linzx <3468845278@qq.com>
2025-07-02 18:33:07 +0800

    feat(admin): 集成 MinIO 对象存储

--- admin/src/main/java/com/l2/config/CorsConfig.java
+++ admin/src/main/java/com/l2/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.l2.config;
 
+import io.minio.MinioClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -24,4 +25,11 @@ public class CorsConfig {
         };
     }
 
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint("http://localhost:9000")
+                .credentials("minioadmin", "minioadmin")
+                .build();
+    }
 }
