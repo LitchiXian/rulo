dd34be0ef30ed88323b186d7676762d16fe96050
LitchiXian <3468845278@qq.com>
2025-07-02 19:39:41 +0800

    feat(ui): 配置 Element Plus图标

--- ui/src/components/HelloWorld.vue
+++ ui/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import {test1, test2} from "../api/web/hello.js";
+import {Check, Delete, Edit, Message, Search, Star} from "@element-plus/icons-vue";
 
 defineProps({
   msg: String,
@@ -98,6 +99,42 @@ const submitReport = () => {
   <div class="report-section">
     <button class="report-btn" @click="handleReportDialogOpen">举报</button>
     <div v-if="reportResult" class="report-message">{{ reportResult }}</div>
+  </div>
+
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+
+  <div class="mb-4">
+    <el-button plain>Plain</el-button>
+    <el-button type="primary" plain>Primary</el-button>
+    <el-button type="success" plain>Success</el-button>
+    <el-button type="info" plain>Info</el-button>
+    <el-button type="warning" plain>Warning</el-button>
+    <el-button type="danger" plain>Danger</el-button>
+  </div>
+
+  <div class="mb-4">
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
+  </div>
+
+  <div>
+    <el-button :icon="Search" circle />
+    <el-button type="primary" :icon="Edit" circle />
+    <el-button type="success" :icon="Check" circle />
+    <el-button type="info" :icon="Message" circle />
+    <el-button type="warning" :icon="Star" circle />
+    <el-button type="danger" :icon="Delete" circle />
   </div>
 </template>
 
