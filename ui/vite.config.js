5d2141896d9dbafeaf3edbd83d32a7cb48bf7267
LitchiXian <3468845278@qq.com>
2025-07-03 09:16:47 +0800

    build(ui): 配置vite启动端口，引入@vueuse/core

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,6 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    port: 5173,
+    // 允许访问的域名
+    host: '0.0.0.0',
+    // 若端口被占用，直接退出
+    strictPort: true,
     // 配置正向代理，用于处理跨域
     proxy: {
       '/api': {
