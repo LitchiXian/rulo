58f5e42b17344d312ca8b57056a8110c1d0f27d1
Linzx <3468845278@qq.com>
2025-07-10 17:53:07 +0800

    feat(api): 添加博客文章相关 API 接口

---
+++ ui/src/api/web/bBlogArticle.js
@@ -0,0 +1,40 @@
+import request from '../request';
+
+export function save(data) {
+    return request({
+        url: '/blog/article/save',
+        method: 'post',
+        data: data
+    });
+}
+
+export function remove(data) {
+    return request({
+        url: '/blog/article/remove',
+        method: 'post',
+        data: data
+    });
+}
+
+export function update(data) {
+    return request({
+        url: '/blog/article/update',
+        method: 'post',
+        data: data
+    });
+}
+
+export function list() {
+    return request({
+        url: '/blog/article/list',
+        method: 'get'
+    });
+}
+
+export function get(data) {
+    return request({
+        url: '/blog/article/get',
+        method: 'get',
+        params: data
+    });
+}
