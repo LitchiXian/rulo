861ea4271aec6b630282b826a5eac2899f5394b4
LitchiXian <3468845278@qq.com>
2025-07-31 22:34:48 +0800

    修改 换行

--- sql/l2.sql
+++ sql/l2.sql
@@ -15,7 +15,8 @@
 */
 
 SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET
+FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for b_blog_article
@@ -29,8 +30,8 @@ CREATE TABLE `b_blog_article`
     `user_id`        bigint(0) UNSIGNED                                            NOT NULL COMMENT '作者ID',
     `user_name`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL     DEFAULT NULL COMMENT '作者名称（冗余存储）',
     `published_time` datetime(0)                                                   NULL     DEFAULT NULL COMMENT '发布时间（当设置为公开时）',
-    `is_published`   tinyint(1) UNSIGNED ZEROFILL                                  NOT NULL DEFAULT 0 COMMENT '是否发布（0=私密，1=公开）',
-    `is_deleted`     tinyint(1) UNSIGNED ZEROFILL                                  NOT NULL DEFAULT 0 COMMENT '逻辑删除标志（0=正常，1=已删除）',
+    `is_published`   tinyint(1) UNSIGNED                                   NOT NULL DEFAULT 0 COMMENT '是否发布（0=私密，1=公开）',
+    `is_deleted`     tinyint(1) UNSIGNED                                   NOT NULL DEFAULT 0 COMMENT '逻辑删除标志（0=正常，1=已删除）',
     `create_id`      bigint(0) UNSIGNED                                            NOT NULL COMMENT '创建人ID',
     `create_time`    datetime(0)                                                   NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
     `update_id`      bigint(0) UNSIGNED                                            NOT NULL COMMENT '更新人ID',
@@ -51,9 +52,9 @@ CREATE TABLE `sys_user`
     `user_name`   varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  NOT NULL COMMENT '用户账号',
     `nick_name`   varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  NOT NULL COMMENT '用户昵称',
     `password`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码（bcrypt哈希结果）',
-    `email`       varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  NULL     DEFAULT NULL COMMENT '用户邮箱',
-    `is_active`   tinyint(3) UNSIGNED ZEROFILL                                  NOT NULL DEFAULT 000 COMMENT '账户状态（0=正常，1=异常）',
-    `is_deleted`  tinyint(3) UNSIGNED ZEROFILL                                  NOT NULL DEFAULT 000 COMMENT '逻辑删除标志（0=正常，1=已删除）',
+    `email`       varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL     DEFAULT NULL COMMENT '用户邮箱',
+    `is_active`   tinyint(3) UNSIGNED                                   NOT NULL DEFAULT 000 COMMENT '账户状态（0=正常，1=异常）',
+    `is_deleted`  tinyint(3) UNSIGNED                                   NOT NULL DEFAULT 000 COMMENT '逻辑删除标志（0=正常，1=已删除）',
     `create_id`   bigint(0) UNSIGNED                                            NOT NULL COMMENT '创建人ID',
     `create_time` datetime(0)                                                   NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
     `update_id`   bigint(0) UNSIGNED                                            NOT NULL COMMENT '更新人ID',
@@ -65,4 +66,5 @@ CREATE TABLE `sys_user`
   COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户信息表'
   ROW_FORMAT = Dynamic;
 
-SET FOREIGN_KEY_CHECKS = 1;
+SET
+FOREIGN_KEY_CHECKS = 1;
