83c2afc20ea61bb5ac2ef611712960510d80416a
LitchiXian <3468845278@qq.com>
2026-04-21 15:54:21 +0800

    remove rulo-ui-client

--- rulo-ui-client/src/util/markdownParser.ts
+++
@@ -1,9 +0,0 @@
-import markdownIt from 'markdown-it';
-
-export default function markdownParser() {
-    return markdownIt({
-        html: true,
-        linkify: true,
-        typographer: true
-    })
-}
